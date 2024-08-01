77201fa314d208b6af042fc5a1e9274822ecc4ff
Dmytro Kurkin <deemson.k@gmail.com>
2024-08-01 21:40:29 +0300

    ForEach & ForIndexedEach for sequences

--- sequence.go
+++ sequence.go
@@ -7,6 +7,21 @@ type Sequence[T any] interface {
 	Iterator() iterator.Iterator[T]
 }
 
+// ForEach is an equivalent for iterator.ForEach for a Sequence.
+func ForEach[T any](sequence Sequence[T], f func(T) bool) bool {
+	return iterator.ForEach(sequence.Iterator(), f)
+}
+
+// ForIndexedEach is ForEach with added index.
+func ForIndexedEach[T any](sequence Sequence[T], f func(int, T) bool) bool {
+	index := 0
+	return ForEach[T](sequence, func(item T) bool {
+		proceed := f(index, item)
+		index++
+		return proceed
+	})
+}
+
 type SizedSequence[T any] interface {
 	Sized
 	Sequence[T]
@@ -15,7 +30,7 @@ type SizedSequence[T any] interface {
 // ToSlice converts a SizedSequence to a slice.
 func ToSlice[T any](sequence SizedSequence[T]) []T {
 	slice := make([]T, 0, sequence.Size())
-	iterator.ForEach(sequence.Iterator(), func(item T) bool {
+	ForEach[T](sequence, func(item T) bool {
 		slice = append(slice, item)
 		return true
 	})
--- sequence_test.go
+++ sequence_test.go
@@ -14,3 +14,22 @@ func TestToSlice(t *testing.T) {
 		t.Fatalf("actual slice %#v is not equal to expected %#v", actual, expected)
 	}
 }
+
+func TestForIndexedEach(t *testing.T) {
+	sliceAdapter := collection.FromItems(1, 2, 3)
+	expectedIndexes := []int{0, 1, 2}
+	expectedSlice := []int{1, 2, 3}
+	actualIndexes := []int{}
+	actualSlice := []int{}
+	collection.ForIndexedEach[int](sliceAdapter, func(index int, item int) bool {
+		actualIndexes = append(actualIndexes, index)
+		actualSlice = append(actualSlice, item)
+		return true
+	})
+	if !reflect.DeepEqual(expectedSlice, actualSlice) {
+		t.Fatalf("actual slice %#v is not equal to expected %#v", actualSlice, expectedSlice)
+	}
+	if !reflect.DeepEqual(expectedIndexes, actualIndexes) {
+		t.Fatalf("actual slice indexes %#v is not equal to expected %#v", actualIndexes, expectedIndexes)
+	}
+}
